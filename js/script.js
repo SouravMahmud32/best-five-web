84865b7bac140e95d60490d732ede952e94313b3
sourav-mahmud <souravmahmud32@gmail.com>
2022-08-22 03:28:55 +0600

    player list added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+const playerListArray = [];
+
+function displayPlayerName(AllPlayerName){
+    console.log(AllPlayerName);
+    const tableBody = document.getElementById('name-display-table');
+    tableBody.innerHTML = '';
+    for(i = 0; i < AllPlayerName.length; i++){
+        const name = playerListArray[i].playerName;
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        `;
+        tableBody.appendChild(tr);
+    }
+}
+
 function addToList(player){
     const playerName = player.parentNode.parentNode.children[0].innerText;
-    console.log(playerName);
+
+    const playerObject = {
+        playerName: playerName
+    }
+    playerListArray.push(playerObject);
+    displayPlayerName(playerListArray);
 }
