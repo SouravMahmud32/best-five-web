f1d9431800ec14bcfb3f0628b05bc08757be2c10
sourav-mahmud <souravmahmud32@gmail.com>
2022-08-22 05:19:36 +0600

    calculation by js added

--- js/script.js
+++ js/script.js
@@ -25,3 +25,37 @@ function addToList(player){
     playerListArray.push(playerObject);
     displayPlayerName(playerListArray);
 }
+
+/* Calculate Part */ 
+
+document.getElementById('btn-calculate').addEventListener('click', function(){
+    const budgetField = document.getElementById('budget-field');
+    const budgetAmountString = budgetField.value;
+    budgetAmount = parseFloat(budgetAmountString);
+    let totalPlayerExpenses = budgetAmount * playerListArray.length;
+    // console.log(totalPlayerExpenses);
+    const playerExpenseTotalElement = document.getElementById('player-expense');
+    const previousPlayerExpenseTotalString = playerExpenseTotalElement.innerText;
+    const newTotalPlayerExpense = parseFloat(previousPlayerExpenseTotalString);
+    playerExpenseTotalElement.innerText = totalPlayerExpenses;
+});
+
+document.getElementById('btn-total').addEventListener('click', function(){
+    const managerField = document.getElementById('manager-field');
+    const managerAmountString = managerField.value;
+    managerAmount = parseFloat(managerAmountString);
+
+    const coachField = document.getElementById('coach-field');
+    const coachAmountString = coachField.value;
+    coachAmount = parseFloat(coachAmountString);
+
+    let totalPlayerExpenses = budgetAmount * playerListArray.length;
+
+    let totalCost = totalPlayerExpenses + managerAmount + coachAmount;
+    // console.log(totalCost);
+
+    const allExpenseTotalElement = document.getElementById('expense-total');
+    const previousAllExpenseTotalString = allExpenseTotalElement.value;
+    const newAllExpenseTotal = parseFloat(previousAllExpenseTotalString);
+    allExpenseTotalElement.innerText = totalCost;
+})
